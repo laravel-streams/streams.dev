7894d6cc15c8c04925c8fbb5fbb76ff3e18a039e
Ryan Thompson <service@aiwebsystems.com>
2020-06-09 21:27:15 -0500

    Streams

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,19 @@
 @section('content')
 
 <div class="h-screen flex justify-center align-items-center bg-black">
-    <div style="max-width: 300px;" class="flex self-center">
-        {!! img('public::img/streams.png') !!}
+    <div style="max-width: 300px;" class="flex flex-col self-center">
+        
+        <div>
+            {!! img('public::img/streams.png') !!}
+        </div>
+
+        {{-- <p class="font-mono text-xs text-white text-center">
+            {{ Arr::random([
+                'A Development Cult',
+                'A Development Stack',
+                'A Development Methodology',
+            ]) }}
+        </p> --}}
     </div>
 </div>
     
