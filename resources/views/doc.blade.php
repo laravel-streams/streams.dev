70e5a952f90bdaa943ef39bf7407c37fdb6f5f41
William Åström <william@pixney.com>
2020-07-06 00:38:31 +0200

    .

--- resources/views/doc.blade.php
+++ resources/views/doc.blade.php
@@ -2,43 +2,45 @@
 
 @section('content')
 
-<div class="grid grid-cols-12 gap-4">
-    <div class="col-span-3">
-        <aside class="o-aside sticky top-0">
-            <p>
-                I am a sticky sidebar
-            </p>
-         
-            <ul x-data="theme()" x-init="init">
-                <li @click="onLight" class="cursor-pointer">
-                    I like the lightness
-                </li>
-                <li @click="onDark" class="cursor-pointer">
-                    I like the darkness
-                </li>
-            </ul>
-          
-            <ul>
-                @foreach (Streams::entries('docs')->get() as $item)
-                <li>
-                    <a href="/docs/{{ $item->id }}">{{ $item->title }}</a>
-                </li>
-                @endforeach
-            </ul>
-        </aside>
-    </div>
-    <div class="col-span-9">
-        <main class="o-main">
-            {{-- <h1>
-                {{ $entry->title }}
-            </h1> --}}
-            @if ($entry->intro)
-                <div class="o-intro">
-                    {!! Str::markdown($entry->intro) !!}
-                </div>
-            @endif
-            {!! Str::markdown($entry->body) !!}
-        </main>
+<div class="container mx-auto">
+    <div class="grid grid-cols-12 gap-4">
+        <div class="col-span-3">
+            <aside class="o-aside sticky top-0">
+                <p>
+                    I am a sticky sidebar
+                </p>
+             
+                <ul x-data="theme()" x-init="init">
+                    <li @click="onLight" class="cursor-pointer">
+                        I like the lightness
+                    </li>
+                    <li @click="onDark" class="cursor-pointer">
+                        I like the darkness
+                    </li>
+                </ul>
+              
+                <ul>
+                    @foreach (Streams::entries('docs')->get() as $item)
+                    <li>
+                        <a href="/docs/{{ $item->id }}">{{ $item->title }}</a>
+                    </li>
+                    @endforeach
+                </ul>
+            </aside>
+        </div>
+        <div class="col-span-9">
+            <main class="o-main">
+                {{-- <h1>
+                    {{ $entry->title }}
+                </h1> --}}
+                @if ($entry->intro)
+                    <div class="o-intro">
+                        {!! Str::markdown($entry->intro) !!}
+                    </div>
+                @endif
+                {!! Str::markdown($entry->body) !!}
+            </main>
+        </div>
     </div>
 </div>
 
