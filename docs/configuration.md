8679b75ba2b146ec2f05c798bb0e0c33e53423b0
Ryan Thompson <service@aiwebsystems.com>
2020-08-20 14:23:51 -0500

    Docs

--- docs/configuration.md
+++ docs/configuration.md
@@ -8,10 +8,8 @@ sort: 2
 - **Explaination:** An elevator pitch that signals the reader to continue or not (keep looking for relavant page).
     - Config files for the Streams platform are located in `config/streams/`. They are PHP files named by area of responsibility.
 - **Sections/Features:** Separate sections/sub-sections (h2s/h3s) consistently. This will build the ToC.
-    - Publishing
-    - Addons
-    - Images
-    - System
+    - Config Files
+    - Environment Variables
 - **Next Steps:** Next actions to take that are intentional versus simply additional reading.
 - **Code Examples:** Code examples and snippets.
 - **Insights:** Tips, post scriptum, creative links.
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,7 @@
 */
 
 use Illuminate\Support\Facades\Route;
-use Anomaly\Streams\Ui\Support\Builder;
 use Anomaly\Streams\Ui\Form\FormBuilder;
-use Anomaly\Streams\Platform\Support\Workflow;
 
 Route::view('/', 'welcome');
 
@@ -28,70 +26,3 @@ Route::any('test/builders', function() {
 //     'stream' => 'docs',
 //     'uses' => '\Anomaly\Streams\Platform\Http\Controller\EntryController@view'
 // ]);
-
-Route::any('/test/foo', function () {
-
-    $builder = new Builder;
-
-    $workflow = (new Workflow([
-        'name' => 'build',
-        'steps' => [
-            'first_step' => function () {
-                \Log::info('First Step Output');
-            },
-            'second_step' => function () {
-                \Log::info('Second Step Output');
-            }
-        ],
-        // 'callback' => function ($callback, $payload) use ($builder) {
-        //     $builder->fire(implode('_', [
-        //         $callback['workflow'],
-        //         $callback['name']
-        //     ]), $payload);
-        // }
-    ]))->passThrough($builder);
-
-    $workflow->steps = array_merge($workflow->steps, ['third_step' => function() {
-        \Log::info('Third (Custom) Step Output');
-    }]);
-    
-    $builder->on('build_after_second_step', function() {
-        \Log::info('AFTER Second Step Output');
-    });
-
-    $builder->on('build_after_third_step', function() {
-        \Log::info('AFTER THIRD (Custom) Step Output');
-    });
-
-    $workflow->process();
-});
-
-// Route::get('/garden', function () {
-
-//     return (new TableBuilder([
-//         'stream' => 'plants',
-//         'filters' => [
-//             'name',
-//             'type',
-//         ],
-//         'columns' => [
-//             'name',
-//             'type',
-//         ],
-//         'buttons' => [
-//             'edit' => [
-//                 'href' => 'garden/edit/{entry.id}'
-//             ],
-//             'view' => [
-//                 'href' => 'garden/{entry.id}'
-//             ],
-//         ],
-//     ]))->response();
-// });
-
-// Route::get('/garden/edit/{id}', function ($id) {
-//     return (new FormBuilder([
-//         'stream' => 'plants',
-//         'entry' => $id,
-//     ]))->response();
-// });
