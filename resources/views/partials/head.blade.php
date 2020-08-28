9136aafa999ae4f9903f1f7f11abcec11c48d31c
Ryan Thompson <service@aiwebsystems.com>
2020-08-28 12:04:54 -0500

    2fa

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -6,6 +6,8 @@
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+<meta name="cf-2fa-verify" content="5491995f81e49ae">
+
 <!-- Favicon -->
 {{-- {!! favicons('public::img/favicon.png') !!} --}}
 
