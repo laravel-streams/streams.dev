cf333a8bd687ccc8a505f4d2b25149b8ce2604b6
Ryan Thompson <service@aiwebsystems.com>
2020-09-13 03:15:42 -0500

    Show me the docs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,7 @@
         <p class="font-mono text-center">
             <a href="https://github.com/anomalylabs/streams" target="_blank" class="btn-outline-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline border border-purple hover:bg-purple text-purple hover:text-white font-normal py-2 px-4 rounded">GitHub</a>
             <a href="https://discord.gg/RjywDG2" target="_blank" class="btn-outline-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline border border-purple hover:bg-purple text-purple hover:text-white font-normal py-2 px-4 rounded">Discord</a>
-            @if (in_array(Request::ip(), ['127.0.0.1', '::1']) || Request::has('showme'))
+            @if (in_array(Request::ip(), ['127.0.0.1', '::1']) || Request::has('showmethedocs'))
             <a href="/docs" class="btn-outline-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline border border-purple hover:bg-purple text-purple hover:text-white font-normal py-2 px-4 rounded">Docs</a>
             @endif
         </p>
