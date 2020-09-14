fdb876c1056c800fcfc980a5607c37f587308a91
Ryan Thompson <ryan@pyrocms.com>
2020-09-14 08:19:25 -0500

    Docs

--- resources/views/doc.blade.php
+++ resources/views/doc.blade.php
@@ -5,14 +5,15 @@
 <div class="o-doc">
     <div class="c-versioning">
         Alpha Version
+        <div class="js-toc"></div>
     </div>
     <style rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/styles/default.min.css"></style>
-    
+
     <div class="flex flex-wrap">
         <div class="xl:w-2/12">
-            
+
             <aside class="o-aside sticky top-0">
-                
+
                 {{-- <ul class="flex text-sm">
                     <li class="cursor-pointer mr-2">
                         Lightness
@@ -34,20 +35,21 @@
                 <br>
 
                 <?php $areas = ['docs' => 'Streams', 'core' => 'Core', 'ui' => 'UI']; ?>
-            
+
                 @if (count(request()->segments()) == 3 && request()->segment(2) == 'core')
-                    <?php $docs = Streams::entries('docs_core')->where('enabled', true)->orderBy('sort', 'asc')->get(); ?>
-                    <?php $suffix = '/core'; ?>
+                <?php $docs = Streams::entries('docs_core')->where('enabled', true)->orderBy('sort', 'asc')->get(); ?>
+                <?php $suffix = '/core'; ?>
                 @elseif (count(request()->segments()) == 3 && request()->segment(2) == 'ui')
-                    <?php $docs = Streams::entries('docs_ui')->where('enabled', true)->orderBy('sort', 'asc')->get(); ?>
-                    <?php $suffix = '/ui'; ?>
+                <?php $docs = Streams::entries('docs_ui')->where('enabled', true)->orderBy('sort', 'asc')->get(); ?>
+                <?php $suffix = '/ui'; ?>
                 @else
-                    <?php $docs = Streams::entries('docs')->where('enabled', true)->orderBy('sort', 'asc')->get(); ?>
-                    <?php $suffix = ''; ?>
+                <?php $docs = Streams::entries('docs')->where('enabled', true)->orderBy('sort', 'asc')->get(); ?>
+                <?php $suffix = ''; ?>
                 @endif
-                
-                <ul>                    
-                    @foreach ($stream->entries()->where('enabled', true)->orderBy('sort', 'ASC')->where('category', null)->get() as $page)
+
+                <ul>
+                    @foreach ($stream->entries()->where('enabled', true)->orderBy('sort', 'ASC')->where('category',
+                    null)->get() as $page)
                     <li>
                         <a href="{{$page->id}}">{{ $page->title }}</a>
                         {{-- <strong>[{{ $page->stage ?: 'outlining' }}]</strong> --}}
@@ -72,7 +74,7 @@
                 @endif
 
                 @endforeach
-                
+
             </aside>
         </div>
         <div class="lg:w-8/12 xl:w-9/12 lg:ml-auto">
@@ -81,14 +83,52 @@
                     {{ $entry->title }}
                 </h1>
                 @if ($entry->intro)
-                    <div class="o-intro">
-                        {!! Str::markdown($entry->intro) !!}
-                    </div>
+                <div class="o-intro">
+                    {!! Str::markdown($entry->intro) !!}
+                </div>
                 @endif
-                {!! Str::markdown($entry->body) !!}
+                <div class="js-toc-content">
+                    {!! Str::markdown($entry->body) !!}
+                </div>
             </main>
         </div>
     </div>
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/anchor-js/anchor.min.js"></script>
+
+<script>
+    anchors.options = {
+        placement: 'right',
+        visible: 'hover',
+        truncate: 64,
+        icon: '#'
+    };
+
+    anchors.add('.js-toc-content h2, .js-toc-content h3, .js-toc-content h4');
+ </script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tocbot/4.11.1/tocbot.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tocbot/4.11.1/tocbot.css">
+
+<script>
+
+    tocbot.init({
+        // Where to render the table of contents.
+        tocSelector: '.js-toc',
+        // Where to grab the headings to build the table of contents.
+        contentSelector: '.js-toc-content',
+        // Which headings to grab inside of the contentSelector element.
+        headingSelector: 'h1, h2, h3',
+        // For headings inside relative or absolute positioned containers within content.
+        hasInnerContainers: false,
+        // How many heading levels should not be collapsed.
+        // For example, number 6 will show everything since
+        // there are only 6 heading levels and number 0 will collapse them all.
+        // The sections that are hidden will open
+        // and close as you scroll to headings within them.
+        collapseDepth: 1,
+    });
+</script>
 
 @endsection
