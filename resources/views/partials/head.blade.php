4a786c650ff93edfff050bd2ee3349a2efd340cc
William Åström <william@pixney.com>
2020-09-17 09:55:08 +0200

    :footprints: Fonts were loading wrong. Fixed.

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -13,5 +13,4 @@
 
 <!-- CSS -->
 <link href="{{ mix('/css/theme.css') }}" rel="stylesheet">
-
-<link href="https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap" rel="stylesheet">
