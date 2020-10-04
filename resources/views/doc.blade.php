0ab671a22d5fcb2d32aaed68235d59bf993c1fa6
William Åström <william@pixney.com>
2020-10-04 17:09:35 +0200

    Adjusted largest breakpoint

--- resources/views/doc.blade.php
+++ resources/views/doc.blade.php
@@ -4,24 +4,26 @@
 @endsection
 @section('content')
 
-<div class="flex flex-wrap min-h-screen mx-auto" style="max-width:1500px;">
-    @include('partials.docs.aside')
+<div class="container mx-auto">
+    <div class="flex flex-wrap min-h-screen mx-auto">
+        @include('partials.docs.aside')
 
-    <div class="o-doc-body">
+        <div class="o-doc-body">
 
 
 
-        @if($entry->intro)
+            @if($entry->intro)
 
-        @include('partials.docs.intro')
+            @include('partials.docs.intro')
 
-        @else
-        <h1>
-            {{ $entry->title }}
-        </h1>
-        @endif
-        {!! Str::markdown(View::parse($entry->body)) !!}
+            @else
+            <h1>
+                {{ $entry->title }}
+            </h1>
+            @endif
+            {!! Str::markdown(View::parse($entry->body)) !!}
+        </div>
+        @include('partials.docs.toc')
     </div>
-    @include('partials.docs.toc')
 </div>
 @endsection
--- resources/views/partials/docs/toc.blade.php
+++ resources/views/partials/docs/toc.blade.php
@@ -1,3 +1,3 @@
-<div class="c-toc-list">
+<div class="c-toc-list w-2/12">
     <div class="c-toc-list__child js-toc"></div>
 </div>
