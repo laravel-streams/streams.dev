1647e1ea4555ab685b92452890ef2b09fb872950
Ryan Thompson <service@aiwebsystems.com>
2020-10-07 11:41:35 -0500

    Tracking

--- resources/views/partials/assets.blade.php
+++ resources/views/partials/assets.blade.php
@@ -14,3 +14,14 @@
 {!! Assets::script('public::assets/js/main.js') !!} --}}
 
 <script src="{{ mix('/js/app.js') }}"></script>
+
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-179956604-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-179956604-1');
+</script>
