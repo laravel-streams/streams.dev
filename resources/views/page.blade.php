8f8ebc6b855a7790069d915e59b6315c1c4eff42
William Åström <william@pixney.com>
2020-10-10 18:45:27 +0200

    Play

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -5,12 +5,11 @@
 @section('content')
 
 
-<div class="container mx-auto">
     <div class="o-page-body w-12/12">
         {!! $entry->body !!}
     </div>
-</div>
+
 <div class="o-footer">
-    
+
 </div>
 @endsection
