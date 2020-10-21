beaa2ea462ec9ededdbb3e4803699bb30c39447a
Ryan Thompson <service@aiwebsystems.com>
2020-10-21 00:59:49 -0500

    CP

---
+++ routes/cp.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+use Illuminate\Support\Facades\Route;
+
+Route::any('test/{stream}', function ($stream) {
+
+    dd($stream);
+});
