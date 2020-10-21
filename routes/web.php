759eb3efdf749449d4dafc70faa8dda8a54c065e
Ryan Thompson <service@aiwebsystems.com>
2020-10-21 02:00:58 -0500

    Routes

--- routes/web.php
+++ routes/web.php
@@ -11,36 +11,6 @@
 |
 */
 
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
-use Streams\Core\Support\Facades\Streams;
 
 Route::streams('/', 'welcome');
-
-Route::any('cp/{stream}', function ($stream) {
-
-    if (App::environment() !== 'local') {
-        abort(404);
-    }
-
-    $stream = Streams::make($stream);
-
-    return $stream
-        ->table()
-        ->response();
-});
-
-Route::any('cp/{stream}/{entry?}', function ($stream, $entry = null) {
-
-    if (App::environment() !== 'local') {
-        abort(404);
-    }
-
-    $stream = Streams::make($stream);
-
-    return $stream
-        ->form([
-            'entry' => $entry
-        ])
-        ->response();
-});
