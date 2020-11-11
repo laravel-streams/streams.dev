6534d9bae8749bceeacab4a186d176fc03d17ece
Ryan Thompson <ryan@pyrocms.com>
2020-11-11 00:03:11 -0600

    Whitelist

--- app/Http/Middleware/AuthenticateCp.php
+++ app/Http/Middleware/AuthenticateCp.php
@@ -15,7 +15,7 @@ class AuthenticateCp
      */
     public function handle($request, \Closure $next)
     {
-        if (!in_array(Request::ip(), ['127.0.0.1', '::1', '65.158.88.194', '162.158.62.94'])) {
+        if (!in_array(Request::ip(), array_merge(['127.0.0.1', '::1', '65.158.88.194', '162.158.62.94'], config('streams.whitelist', [])))) {
             return abort(403);
         }
 
