1ad7aa481bf22d29738a4cbaa4e1f376288f0cc7
Ryan Thompson <ryan@pyrocms.com>
2020-12-16 14:54:22 -0600

    Stuff

--- examples/simple_users.md
+++ examples/simple_users.md
@@ -1,17 +1,22 @@
 ---
+__created_at: 1608143450
+__updated_at: 1608143450
 example_title: 'Simple Users'
-example_datetime: '2020-10-05 15:38:33'
+example_datetime: '2020-12-16 20:52:49'
 example_textarea: 'A simple description will do.'
 example_content: 'Some content'
 example_color: '#8147eb'
 example_slug: simple-website
 example_select: hard
-example_radio: second
+example_radio: third
 title: ''
 example_markdown: "# Yay!\r\nStyles are no longer colliding here :-)"
-example_date: '2020-10-05'
-example_time: '15:38:33'
+example_date: '2020-12-16'
+example_time: '20:52:49'
 example_range: 39
+example_boolean: true
+example_image: resources/img/deftones.jpg
+example_file: dummy.pdf
+example_relationship: introduction
 ---
-
-Users
+{!! Storage::url($entry->example_file) !!}
--- resources/views/example.blade.php
+++ resources/views/example.blade.php
@@ -4,41 +4,17 @@
 
 <div class="">
     <div class="grid grid-cols-12 gap-4">
-        <div class="col-span-2">
-            <aside class="o-aside sticky top-0">
-                
-                <ul >
-                    <li class="cursor-pointer">
-                        I like the lightness s
-                    </li>
-                    <li class="cursor-pointer">
-                        I like the darkness
-                    </li>
-                </ul>
-                <ul>
-                    @foreach (Streams::entries('examples')->orderBy('sort', 'asc')->get() as $item)
-                    <li>
-                        <a href="/examples/{{ $item->id }}" title="{{ $item->linkTitle ?? $item->title }}">
-                            {{ $item->linkTitle ?? $item->title }}
-                        </a>
-                    </li>
-                    @endforeach
-                </ul>
-            </aside>
-        </div>
-        <div class="col-span-8 col-start-4">
-            <main class="o-main">
-                <h1>
-                    {{ $entry->title }}
-                </h1>
-                @if ($entry->intro)
-                    <div class="o-intro">
-                        {!! Str::markdown($entry->intro) !!}
-                    </div>
-                @endif
-                {!! Str::markdown(View::parse($entry->body, compact('entry'))) !!}
-            </main>
-        </div>
+        <main class="o-main">
+            <h1>
+                {{ $entry->title }}
+            </h1>
+            @if ($entry->intro)
+                <div class="o-intro">
+                    {!! Str::markdown($entry->intro) !!}
+                </div>
+            @endif
+            {!! Str::markdown(View::parse($entry->body, compact('entry'))) !!}
+        </main>
     </div>
 </div>
 
