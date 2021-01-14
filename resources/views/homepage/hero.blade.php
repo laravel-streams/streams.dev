0d890719d35fc8aa7dac807e407446b9fb15c8fb
Ryan Thompson <service@aiwebsystems.com>
2021-01-14 14:20:38 -0600

    Meh

--- resources/views/homepage/hero.blade.php
+++ resources/views/homepage/hero.blade.php
@@ -3,5 +3,10 @@
         <h1 class="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight text-gray-900 mt-10 mb-8 sm:mt-14 sm:mb-10">{!! __('app.title') !!}</h1>
     
         <p class="max-w-screen-lg text-lg text-gray-700 sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-11">{!! __('app.description') !!} {{-- <a href="/mission#your-team-and-clients-will-love-it" class="font-bold text-black border-b-2 border-black border-dotted" onmouseover="this.classList.remove('border-dotted');" onmouseout="this.classList.add('border-dotted');">Your team and clients will love it.</a> --}}</p>    
+
+        {{-- <div class="mt-16 space-x-4">
+            <a href="/explore" class="px-6 py-3 rounded-3xl font-bold text-2xl bg-black hover:bg-gray-800 text-white outline-none focus:outline-none hover:shadow-md transition duration-200 ease-in-out">Explore</a>
+        </div> --}}
+
     </div>    
 </div>
