f0dcfb9ed167af989912bd3d252a780177e08881
Ryan Thompson <ryan@pyrocms.com>
2021-01-23 01:00:55 -0600

    Ya

--- server_deploy.sh
+++ server_deploy.sh
@@ -4,7 +4,7 @@ set -e
 echo "Deploying application..."
 
 # Enter maintenance mode
-(php artisan down || true
+(php artisan down) || true
     
     # Update codebase
     git fetch origin production
