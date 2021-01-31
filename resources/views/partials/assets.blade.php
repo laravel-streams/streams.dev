eaaf4af048024ad9006ce5a358ca71a69b60bb50
Ryan Thompson <ryan@pyrocms.com>
2021-01-30 19:47:57 -0600

    Der

--- resources/views/partials/assets.blade.php
+++ resources/views/partials/assets.blade.php
@@ -16,3 +16,13 @@
         return app.start();
     });
 </script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-179956604-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-179956604-1');
+</script>
