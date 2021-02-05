92c7aa83cc9e16a43fdc580ba08fd5a3f4b6421a
Ryan Thompson <service@aiwebsystems.com>
2021-02-05 11:31:08 -0600

    Ugh

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,8 +2,8 @@ const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
 mix
-    .ts('resources/js/app.ts', 'js')
     .sass('resources/scss/theme.scss', 'css')
+    .ts('resources/js/app.ts', 'js')
     .options({
         processCssUrls: false,
         postCss: [tailwindcss('./tailwind.config.js')],
