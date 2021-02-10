da953d5d638735cf2c8943aff17d7cf69fe80a06
Ryan Thompson <service@aiwebsystems.com>
2021-02-10 00:30:27 -0600

    update

--- resources/views/partials/assets.blade.php
+++ resources/views/partials/assets.blade.php
@@ -1,6 +1,6 @@
 {!! Assets::tag('/vendor/streams/core/js/index.js') !!}
-{{-- {!! Assets::tag('/vendor/streams/ui/js/index.js') !!} --}}
-{{-- {!! Assets::tag('/vendor/streams/api/js/api.js') !!} --}}
+{!! Assets::tag('/vendor/streams/api/js/index.js') !!}
+{!! Assets::tag('/vendor/streams/ui/js/index.js') !!}
 
 {!! Assets::tag('/js/app.js') !!}
 
@@ -9,7 +9,8 @@
     window.streams.core.app.bootstrap({
         providers: [
             window.streams.core.StreamsServiceProvider,
-            //window.streams.api.ApiServiceProvider,
+            window.streams.api.ApiServiceProvider,
+            window.streams.ui.UiServiceProvider,
             window.AppServiceProvider
         ]
     }).then(app => {
