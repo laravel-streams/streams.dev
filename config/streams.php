33064a4cb87caf4e5e8dae9e6e0cbadeb7606207
Ryan Thompson <ryan@pyrocms.com>
2021-05-11 23:18:09 -0500

    Whitelist

--- config/streams.php
+++ config/streams.php
@@ -4,7 +4,7 @@ return [
     'whitelist' => [
         '::1',
         '127.0.0.1',
-        '193.148.18.54',
+        '216.175.28.99',
     ],
     'cp' => [
         'prefix' => env('STREAMS_CP_PREFIX', 'cp'),
