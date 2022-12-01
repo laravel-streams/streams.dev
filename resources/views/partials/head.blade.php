02b18bb0ebc24ef844b0d006734e6c5651b33d2b
Ryan Thompson <ryan@pyrocms.com>
2022-12-01 00:21:46 -0600

    Initial commit

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>{{ config('app.name') }}</title>
+
+<link rel="icon" type="image/png" href="favicon.png" />
+
+@vite(['resources/js/app.js'])
